717f7b534fbb004c60d577c99dac9306d2bdc1cb
Mayur Khandave <mayusuk@gmail.com>
2019-04-30 22:55:30 -0700

    integrated the image matching code

--- main.py
+++ main.py
@@ -1,14 +1,28 @@
 import logging
-
-from flask import Flask
-
+import time
+import os
+from flask import Flask, request
+from matching import CNN
 
 app = Flask(__name__)
+UPLOAD_FOLDER = os.path.basename('uploads')
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
 @app.route('/')
 def hello():
+    time.sleep(4)
     return 'Hello World!'
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    file = request.files['image']
+    f = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+    
+    # add your custom code to check that the uploaded file is a valid image and not a malicious file (out-of-scope for this post)
+    file.save(f)
+    model = CNN()
+    return str(model.match(f))
 
 
 @app.errorhandler(500)
---
+++ matching.py
@@ -0,0 +1,110 @@
+import numpy as np
+from scipy import misc
+import matplotlib
+import matplotlib.pyplot as plt
+import sys
+sys.path.insert(0, '$CAFFE_ROOT/python')
+import caffe
+import os
+import pickle
+import math
+import csv
+import copy
+import allsharp_source.allsharp_selective_search as ss
+from operator import add
+from collections import defaultdict
+# set constants
+
+
+class CNN:
+    def __init__(self):
+
+        self.img_size = 224
+        self.output_blob_name = "fc8"
+
+        pretrained = 'allsharp_source/allsharp_vgg/allsharp_VGG_ILSVRC_16_layers.caffemodel'
+        model = 'allsharp_source/allsharp_vgg/allsharp_VGG_ILSVRC_16_layers_deploy.prototxt'
+
+        # initialize input image array
+        self.dr_results = []
+        self.dr_names = []
+        self.dc_names = []
+
+        caffe.set_mode_cpu()
+        self.net = caffe.Net(model, pretrained, caffe.TEST)
+
+        self.transformer = caffe.io.Transformer({'data': self.net.blobs['data'].data.shape})
+        self.transformer.set_transpose('data', (2,1,0))
+
+        with open('dr_names.pickle', 'rb') as handle:
+            self.dr_names = pickle.load(handle)
+
+        with open('filename.pickle', 'rb') as handle:
+            self.dr_results = pickle.load(handle)
+        self.dic = defaultdict(int)
+
+    def match(self, dc_filepath):
+
+        dc_name = dc_filepath.split('/')[-1]
+        self.dc_names.append(dc_name)
+        img = ss.get_ss_crop(dc_filepath)
+        img = misc.imresize(img, (self.img_size, self.img_size))
+
+        input_data = self.transformer.preprocess('data', img)
+        self.net.blobs['data'].data[...] = np.array(input_data)
+        output = self.net.forward()
+        input_result = output[self.output_blob_name]
+
+        # find the distances
+        distances = [np.linalg.norm(input_result-dr_result) for dr_result in self.dr_results]
+
+        # calculate ranking
+        dist_array = np.array(distances)
+        
+        sorted_indices = dist_array.argsort().tolist()
+        rank_lookup = {}
+        rank = 1
+        for i in sorted_indices:
+            rank_lookup[self.dr_names[i]] = rank
+            rank = rank + 1
+        
+        dr_reranked = []
+        for dr_name, dr_rank in rank_lookup.iteritems():
+            if dr_name in dr_reranked:
+                continue
+                
+            # find the other name
+            if "SF" in dr_name:
+                dr_counterpart = dr_name.replace("SF", "SB")
+            elif "SB" in dr_name:
+                dr_counterpart = dr_name.replace("SB", "SF")
+            else:
+                dr_counterpart = dr_name
+                
+            if dr_counterpart in dr_reranked:
+                continue
+                
+            if rank_lookup[dr_counterpart] < rank_lookup[dr_name]:
+                #print dr_counterpart + "=" + str(rank_lookup[dr_counterpart]) + " has lower rank than this image, " + dr_name + "=" + str(rank_lookup[dr_name])
+                rank_lookup[dr_name] = rank_lookup[dr_counterpart] + 0.5
+            elif rank_lookup[dr_name] < rank_lookup[dr_counterpart]:
+                rank_lookup[dr_counterpart] = rank_lookup[dr_name] + 0.5
+            else:
+                print "error! ranks are the same between image and counterpart."                
+            dr_reranked.append(dr_name)
+        
+        adjusted_ranks = []
+        for dr_name in self.dr_names:
+            adjusted_ranks.append(rank_lookup[dr_name])
+        rank_to_dr = {v: k for k, v in rank_lookup.iteritems()}
+        self.dic[dc_name] = rank_to_dr[1]  
+
+        rerank_dist_array = np.array(adjusted_ranks)
+        sorted_indices = rerank_dist_array.argsort()
+        ranks = np.empty(len(rerank_dist_array),int)
+        ranks[sorted_indices] = np.add(np.arange(len(dist_array)), 1).tolist()
+
+        for key, val in self.dic.iteritems():
+            print str(key) + " : "+ val 
+        print 'done.'
+        return self.dic
